28a3ab45620551cda9b76d36669c066e0d60a3bb
Régis Gabetta <regis.gabetta@wavy.global>
2019-05-07 22:44:04 -0300

    pacman visual

---
+++ src/com/ic/unicamp/br/mc322/pacman/Pacman.java
@@ -0,0 +1,33 @@
+package src.com.ic.unicamp.br.mc322.pacman;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Pacman extends JFrame {
+
+    public Pacman() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        add(new Board());
+               
+        setResizable(false);
+        pack();
+        
+        setTitle("Pacman");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Pacman();
+            ex.setVisible(true);
+        });
+    }
+}
--- src/com/ic/unicamp/br/mc322/pacman/Runner.java
+++
@@ -1,8 +0,0 @@
-package src.com.ic.unicamp.br.mc322.pacman;
-
-public class Runner {
-	public static void main(String[] args) {
-		Game g = new Game();
-		g.start();
-	}
-}
