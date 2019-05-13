b35af7ab08aa22c9358a7697570cb01d2aaebab9
m4rquee <lucasxm12@gmail.com>
2019-05-13 14:14:04 -0300

    level class

--- src/com/ic/unicamp/br/mc322/pacman/game/controller/LevelController.java
+++ src/com/ic/unicamp/br/mc322/pacman/game/controller/LevelController.java
@@ -1,5 +1,7 @@
 package com.ic.unicamp.br.mc322.pacman.game.controller;
 
+import com.ic.unicamp.br.mc322.pacman.game.gameobject.Board;
+
 public class LevelController {
 	private Board current;
 
---
+++ src/com/ic/unicamp/br/mc322/pacman/game/gameobject/Board.java
@@ -0,0 +1,7 @@
+package com.ic.unicamp.br.mc322.pacman.game.gameobject;
+
+public class Board {
+
+	private GameObject[][] objects;
+
+}
